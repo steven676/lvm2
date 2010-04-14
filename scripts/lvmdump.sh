8dced85ca8b08fdd30fd55d1719566f8b396fc06
agk <agk>
2010-04-14 20:03:46 +0000

    touch file

--- scripts/lvmdump.sh
+++ scripts/lvmdump.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # We use some bash-isms (getopts?)
 
-# Copyright (C) 2007 Red Hat, Inc. All rights reserved.
+# Copyright (C) 2007-2010 Red Hat, Inc. All rights reserved.
 #
 # This file is part of LVM2.
 #
