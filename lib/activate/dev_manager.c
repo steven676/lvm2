steven676/lvm2

commit 02f6f4902fd90709f55e2e97e969ee28c2945c81

Disable partial activation for thin LVs and LVs with all missing segments
Count number of error and existing areas and if there is no existing area
for the LV avoid its activation.

Always disable partial activatio for thin volumes.

For mirrors currently put in hack to let it pass with a special name
since current mirror code needs to activate such LV during some operations.

lib/activate/dev_manager.c
@@ -1373,6 +1373,8 @@ int add_areas_line(struct dev_manager *dm, struct lv_segment *seg,
 	char *dlid;
 	struct stat info;
 	const char *name;
+	unsigned num_error_areas = 0;
+	unsigned num_existing_areas = 0;
 
 	/* FIXME Avoid repeating identical stat in dm_tree_node_add_target_area */
 	for (s = start_area; s < areas; s++) {
@@ -1388,10 +1390,12 @@ int add_areas_line(struct dev_manager *dm, struct lv_segment *seg,
 			}
 			if (!_add_error_area(dm, node, seg, s))
 				return_0;
+			num_error_areas++;
 		} else if (seg_type(seg, s) == AREA_PV) {
 			if (!dm_tree_node_add_target_area(node, dev_name(seg_dev(seg, s)), NULL,
 				    (seg_pv(seg, s)->pe_start + (extent_size * seg_pe(seg, s)))))
 				return_0;
+			num_existing_areas++;
 		} else if (seg_is_raid(seg)) {
 			/*
 			 * RAID can handle unassigned areas.  It simple puts
@@ -1432,6 +1436,28 @@ int add_areas_line(struct dev_manager *dm, struct lv_segment *seg,
 		} else {
 			log_error(INTERNAL_ERROR "Unassigned area found in LV %s.",
 				  seg->lv->name);
+			return 0;
+		}
+	}
+
+        if (num_error_areas) {
+		/* Thins currently do not support partial activation */
+		if (lv_is_thin_type(seg->lv)) {
+			log_error("Cannot activate %s%s: pool incomplete.",
+				  seg->lv->vg->name, seg->lv->name);
+			return 0;
+		}
+
+		/*
+		 * Mirrors activate LVs replaced with error targets
+		 *
+		 * TODO: Can we eventually skip to activate such LVs ?
+		 */
+		if (!num_existing_areas &&
+		    !strstr(seg->lv->name, "_mimage_") &&
+		    !((name = strstr(seg->lv->name, "_mlog")) && !name[5])) {
+			log_error("Cannot activate %s/%s: all segments missing.",
+				  seg->lv->vg->name, seg->lv->name);
 			return 0;
 		}
 	}

WHATS_NEW
@@ -1,5 +1,6 @@
 Version 2.02.90 - 
 ===================================
+  Disable partial activation for thin LVs and LVs with all missing segments.
   Do not print warning for pv_min_size set in range between 512KB and 2MB.
   Clean up systemd unit ordering and requirements.
   Fix lcov reports when srcdir != builddir.

commit 7edfcbec065e03d0e14deef53640ec5b9df258e5

Fix bug that caused RAID devices to be unable to activate if sub-LV was missing.
Commit 02f6f4902fd90709f55e2e97e969ee28c2945c81 introduced a bug that caused
RAID devices to fail to activate if the device for a single sub-LV failed.
The special case of LVM mirror was handled, but not LVM RAID.
EXAMPLE:
[root@bp-01 ~]# devices vg
  LV            Copy%  Devices
  lv            100.00 lv_rimage_0(0),lv_rimage_1(0)
  [lv_rimage_0]        /dev/sde1(1)
  [lv_rimage_1]        /dev/sdh1(1)
  [lv_rmeta_0]         /dev/sde1(0)
  [lv_rmeta_1]         /dev/sdh1(0)
[root@bp-01 ~]# vgchange -an vg
  0 logical volume(s) in volume group "vg" now active
[root@bp-01 ~]# off.sh sdh
Turning off sdh
[root@bp-01 ~]# vgchange -ay vg --partial
  Partial mode. Incomplete logical volumes will be processed.
  Couldn't find device with uuid fbI0YO-GX7x-firU-Vy5o-vzwx-vAKZ-feRxfF.
  Cannot activate vg/lv_rimage_1: all segments missing.
  0 logical volume(s) in volume group "vg" now active

AFTER this patch:
[root@bp-01 ~]# vgchange -ay vg --partial
  Partial mode. Incomplete logical volumes will be processed.
  Couldn't find device with uuid fbI0YO-GX7x-firU-Vy5o-vzwx-vAKZ-feRxfF.
  1 logical volume(s) in volume group "vg" now active
[root@bp-01 ~]# devices vg
  Couldn't find device with uuid fbI0YO-GX7x-firU-Vy5o-vzwx-vAKZ-feRxfF.
  LV            Copy%  Devices
  lv            100.00 lv_rimage_0(0),lv_rimage_1(0)
  [lv_rimage_0]        /dev/sde1(1)
  [lv_rimage_1]        unknown device(1)
  [lv_rmeta_0]         /dev/sde1(0)
  [lv_rmeta_1]         unknown device(0)
[root@bp-01 ~]# dmsetup table vg-lv; dmsetup status vg-lv
0 1024000 raid raid1 3 0 region_size 1024 2 253:2 253:3 - -
0 1024000 raid raid1 2 AD 1024000/1024000

No WHATSNEW update necessary because this is an intrarelease fix.

 brassow

lib/activate/dev_manager.c
@@ -1449,11 +1449,14 @@ int add_areas_line(struct dev_manager *dm, struct lv_segment *seg,
 		}
 
 		/*
-		 * Mirrors activate LVs replaced with error targets
+		 * Mirrors activate LVs replaced with error targets and
+		 * RAID can handle non-accessible sub-LVs.
 		 *
 		 * TODO: Can we eventually skip to activate such LVs ?
 		 */
 		if (!num_existing_areas &&
+		    !strstr(seg->lv->name, "_rmeta_") &&
+		    !strstr(seg->lv->name, "_rimage_") &&
 		    !strstr(seg->lv->name, "_mimage_") &&
 		    !((name = strstr(seg->lv->name, "_mlog")) && !name[5])) {
 			log_error("Cannot activate %s/%s: all segments missing.",